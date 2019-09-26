ff4ce088de3fe568bd29f9f4ca38e6c676db118c
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-09-26 01:23:09 -0600

    Prevent double injection

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -806,10 +806,6 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
       super.onPageFinished(webView, url);
 
       if (!mLastLoadFailed) {
-        RNCWebView reactWebView = (RNCWebView) webView;
-
-        reactWebView.callInjectedJavaScript();
-
         emitFinishEvent(webView, url);
       }
     }
